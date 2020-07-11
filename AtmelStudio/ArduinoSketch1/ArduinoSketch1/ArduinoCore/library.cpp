a99427b0613d93313a611333a9db0bd74b604304
spinorkit <kit.adams@gmail.com>
2020-07-12 10:24:06 +1200

    Initial commit of Atmel studion solution for Adafruit Feather M0 Express.

---
+++ AtmelStudio/ArduinoSketch1/ArduinoSketch1/ArduinoCore/library.cpp
@@ -0,0 +1,15 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 11/07/2020 11:08:31 PM
+ * Author : Kit
+ */ 
+
+#include "sam.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
