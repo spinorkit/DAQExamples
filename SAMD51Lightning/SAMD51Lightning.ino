2be6d49a147c7ce1f2d0c2c3357fd85d14621ec1
spinorkit <kit.adams@gmail.com>
2020-08-10 00:00:30 +1200

    Added diagnostics and potentially useful functions for setting PLLs and GCLKs (unused)

--- SAMD51Lightning/SAMD51Lightning.ino
+++ SAMD51Lightning/SAMD51Lightning.ino
@@ -2,8 +2,11 @@
 
 Adafruit_ZeroTimer adcTimer(4);
 
+#ifdef TIMER_OUTPUT_FOR_TEST
+Adafruit_ZeroTimer zt3(3); //Testing
+#endif
 
-/* Valid PWM outs:
+/* Valid PWM outs (for Adafruit Feather ):
 
 FOR SAMD21:
   Timer3: channel 0 on D2 or D10, channel 1 on D5 or D12
@@ -35,8 +38,14 @@ FOR SAMD51:
 #endif
 
 //#define ENABLE_ADCTIMER_PWMOUT 1
+//#define TIMING_CHECK 1
 
+#ifdef TIMING_CHECK
+const int kDefaultADCPointsPerSec = 1;//1024;//100; //~5000 max with 2 samples (1 point) per packet
+#else
 const int kDefaultADCPointsPerSec = 100; //~5000 max with 2 samples (1 point) per packet
+#endif
+
 int gADCPointsPerSec = kDefaultADCPointsPerSec; //~5000 max with 2 samples (1 point) per packet
 
 const int kNSampleRates = 6;
@@ -44,9 +53,178 @@ const int kSampleRates[] = {4000, 2000, 1000, 400, 250, 100};
 
 
 const int kADCStartChan = 2; //A1
+
+#ifdef TIMING_CHECK
+const int kADCChannels = 1;//2; 
+#else
 const int kADCChannels = 2; 
+#endif
 
 const int kADCEndChan = kADCStartChan + kADCChannels;
+
+ // =====================================================================
+ //     CLOCK
+ // ---------------------------------------------------------------------
+ void setupPLL(uint16_t which, uint16_t PllFactFra, uint16_t PllFactInt)
+ {
+	 // ----------------------------------------------------------------------------------------
+	 // ----------------------------------------------------------------------------------------
+	 // PHASE LOCKED LOOP 0 to MCU Clock
+	 // contrary to Start's approach, seems like we should get PLL humming before
+	 // changing GCLK.
+	 OSCCTRL->Dpll[which].DPLLCTRLA.bit.ENABLE = 0;
+	 while (OSCCTRL->Dpll[which].DPLLSYNCBUSY.bit.ENABLE)
+	 ;
+
+	 OSCCTRL->Dpll[which].DPLLRATIO.reg = (PllFactFra<<16) + PllFactInt;  //fraction and int for 100MHz = 24, 0xbea
+	 while (OSCCTRL->Dpll[which].DPLLSYNCBUSY.bit.DPLLRATIO)
+	 ;
+
+	 OSCCTRL->Dpll[which].DPLLCTRLB.bit.DIV = 0; // not meaningful for 32KHz
+	 OSCCTRL->Dpll[which].DPLLCTRLB.bit.DCOEN = 0;
+	 OSCCTRL->Dpll[which].DPLLCTRLB.bit.LBYPASS = 1;	// errata, slow reference clock need to bypass lock lost
+	 OSCCTRL->Dpll[which].DPLLCTRLB.bit.LTIME = 0;
+	 OSCCTRL->Dpll[which].DPLLCTRLB.bit.REFCLK = 1;	// reference clock is 32KHz external
+	 OSCCTRL->Dpll[which].DPLLCTRLB.bit.WUF = 0;
+
+
+	 OSCCTRL->Dpll[which].DPLLCTRLA.bit.ONDEMAND = 0;
+	 OSCCTRL->Dpll[which].DPLLCTRLA.bit.RUNSTDBY = 1;
+	 OSCCTRL->Dpll[which].DPLLCTRLA.bit.ENABLE = 1;
+	 while (OSCCTRL->Dpll[which].DPLLSYNCBUSY.bit.ENABLE)
+	 ;
+
+	 while (! (OSCCTRL->INTFLAG.bit.DPLL0LDRTO || OSCCTRL->Dpll[which].DPLLSTATUS.bit.CLKRDY))
+	 ;
+
+ }
+
+ void FatalError(const char *msg, uint32_t freq)
+ {
+ Serial.print(msg);
+ Serial.println(freq);
+ }
+
+ uint32_t myMCU_CLOCK = 0;
+
+ // =============================================================================
+ // 32768 slow clock using external oscillator.
+ // GCLK3 feeds 32768 so FLL could be used
+ // FDPLL multiplies 32768 to PllFreq_Hz
+ // GCLK0 divides Pll0Freq to McuFreq
+ // GCLK1 divides Pll1Freq by 1
+ //
+ // Freed from Start, which means anything clocked needs to be.
+ void setMyClocks(uint32_t PllFreq_Hz, uint32_t McuFreqHz)
+ {
+	 
+	 // BUG in Start, PLL must be >96MHz but they let you configure 80 and even 20MHz...
+	 // 54.12.5 Fractional Digital Phase Lock Loop (FDPLL) Characteristics
+	 // Table 54-48. Fractional Digital Phase Lock Loop Characteristics
+	 // Symbol  Min. Typ. Max. Units
+	 // fIN     32    -   3200 kHz     using 32.768
+	 // fOUT    96    -    200 MHz     using 120MHz (can post divide)
+	 // yet the manual even gives a 48MHz example
+	 
+	 // If possible, we can use the post divider to get 376471 Hz
+	 // using a legal PLL and post divider.  if illegal, trap
+	 if (PllFreq_Hz < (96000254/255) || PllFreq_Hz > 200000000)
+	    FatalError("ERR_CLOCK_PLL", PllFreq_Hz);
+	 
+	 uint32_t PllDivOut = 1, targetFreq = PllFreq_Hz;
+	 
+	 while (targetFreq < 96000000 && targetFreq <= 200000000 && PllDivOut < 256)
+	 {
+		 PllDivOut++;
+		 targetFreq = PllFreq_Hz*PllDivOut;
+	 }
+	 
+	 if (targetFreq > 200000000 || PllDivOut >= 256)
+	   FatalError("ERR_CLOCK_TGT", targetFreq);
+	 
+	 
+	 if (McuFreqHz > 120000000)
+	 McuFreqHz = 120000000;
+	 if (McuFreqHz > PllFreq_Hz)
+	 McuFreqHz = PllFreq_Hz;
+	 
+	 myMCU_CLOCK = McuFreqHz;
+
+	 uint32_t PllFactInt = targetFreq/32768 - 1;
+	 uint32_t PllFactFra = (32*(targetFreq - 32768*(PllFactInt+1)))/32768;
+	 
+	 // ----------------------------------------------------------------------------------------
+	 // WAIT STATES
+	 // BUG in Start - lets you set clocks without auto or valid wait states.
+	 // 54.11 conservatively for 1.8V
+	 uint32_t waits = 0;
+	 if (McuFreqHz >=  22000000) waits++;
+	 if (McuFreqHz >=  44000000) waits++;
+	 if (McuFreqHz >=  67000000) waits++;
+	 if (McuFreqHz >=  89000000) waits++;
+	 if (McuFreqHz >= 111000000) waits++;
+	 if (McuFreqHz >= 120000000) waits++;
+	 NVMCTRL->CTRLA.bit.RWS = waits; // allows 111MHz at 1.8V
+
+	 // ----------------------------------------------------------------------------------------
+	 // Now that we know we CAN do this, reset clocks
+	 GCLK->CTRLA.bit.SWRST;
+	 while (GCLK->SYNCBUSY.bit.SWRST)
+	 ;
+
+	 // ----------------------------------------------------------------------------------------
+	 // MASTER CLOCK
+	 // Master clock divide by 1.
+	 MCLK->CPUDIV.reg = MCLK_CPUDIV_DIV_DIV1_Val;
+
+	 
+	 // ----------------------------------------------------------------------------------------
+	 // ----------------------------------------------------------------------------------------
+	 // EXTERNAL 32KHZ CLOCK
+	 // To guarantee the XOSC32K behavior in crystal mode, PC00 must be static.
+	 // PC00 is not pinned out in smaller chips
+	 OSC32KCTRL->XOSC32K.bit.CGM = 01;		// normal drive
+	 OSC32KCTRL->XOSC32K.bit.XTALEN = 0;     // using OSC, so SHOULDN'T run drive
+	 OSC32KCTRL->XOSC32K.bit.EN32K = 1;      // seems needed and there is no gpio for this.  Internal signal?
+	 OSC32KCTRL->XOSC32K.bit.ONDEMAND = 0;
+	 OSC32KCTRL->XOSC32K.bit.RUNSTDBY = 1;	// always on
+	 OSC32KCTRL->XOSC32K.bit.ENABLE = 1;		// enabled
+
+	 OSC32KCTRL->CFDCTRL.bit.CFDEN = 0;
+	 OSC32KCTRL->EVCTRL.bit.CFDEO = 0;
+
+	 // no setting up PLL until there is a phase to lock a loop to
+	 // FDPLL has this errata but it looks like osc32K does too,  use INTFLAG, not STATUS
+	 while (!OSC32KCTRL->INTFLAG.bit.XOSC32KRDY)
+	 ;
+
+	 // RTCC from a real clock
+	 OSC32KCTRL->RTCCTRL.bit.RTCSEL = OSC32KCTRL_RTCCTRL_RTCSEL_XOSC32K_Val;
+	 
+	 // ----------------------------------------------------------------------------------------
+	 // IF DEBUG, OUTPUT 32K CLOCK, either way generate it
+	 // GCLK4 echoes OSC32K but can be used in other places.
+	 // GCLK2, 3, and 6 do NOT seem to output properly
+	 GCLK->GENCTRL[4].reg = (1 << 16) | (0x21 << 8) | 5; // 32K = 5, output, enabled and standby = 0x29, divide by 1
+	 while (GCLK->SYNCBUSY.bit.GENCTRL4)
+	 ;
+
+	 setupPLL(0, PllFactFra, PllFactInt);
+	 setupPLL(1, PllFactFra, PllFactInt); // initially 120MHz
+
+	 // GCLK's to the real system.  0 is MCLK and 1 is ADCclk
+	 // Cannot output these clocks
+	 GCLK->GENCTRL[0].reg = (PllDivOut << 16) | (0x21 << 8) | 7; // dpll0 = 7, enabled and standby = 0x21, divide by 1
+	 while (GCLK->SYNCBUSY.bit.GENCTRL0)
+	 ;
+
+	 // in debug, outputs on the switch.
+	 GCLK->GENCTRL[1].reg = (PllDivOut << 16) | (0x21 << 8) | 8; // dpll1 = 8, enabled and standby = 0x21, divide by 100
+	 while (GCLK->SYNCBUSY.bit.GENCTRL1)
+	 ;
+	 
+	 // GCLK[3] and GCLK[6] do not seem to work as planned
+ }
 
 
 void debugNewLine()
@@ -338,16 +516,47 @@ pinMode(6, OUTPUT); //Test only - toggles on each ADC_Handler()
 pinMode(LED_BUILTIN, OUTPUT);
 digitalWrite(LED_BUILTIN, LOW); 
 
+#ifdef TIMER_OUTPUT_FOR_TEST
+  /********************* Timer #3, 16 bit, one PWM out, period = 1024 */
+  zt3.configure(TC_CLOCK_PRESCALER_DIV1, // prescaler
+                TC_COUNTER_SIZE_16BIT,   // bit width of timer/counter
+                TC_WAVE_GENERATION_MATCH_PWM // frequency or PWM mode
+                );
+  const uint32_t kTicks = VARIANT_GCLK1_FREQ/1024;
+  zt3.setPeriodMatch(kTicks - 1, kTicks/4);      // channel 1 only, 200/1000 count
+  if (! zt3.PWMout(true, 1, TIMER3_OUT1)) {
+    Serial.println("Failed to configure PWM output");
+  }
+
+#ifdef _VARIANT_SAMD51_THING_PLUS_
+//Sparkfun Thing Plus has different pin mapping from Adafruit Feather M4
+PORT->Group[0].PINCFG[15].reg = PORT_PINCFG_PMUXEN; //PA15
+PORT->Group[0].PMUX[7].reg &= ~(PORT_PMUX_PMUXO_Msk);
+PORT->Group[0].PMUX[7].reg |= 0x04 << 4;//   PORT_PMUX_PMUXO_E;
+#endif
+//  PORT->Group[g_APinDescription[ulPin].ulPort].DIRSET.reg = (uint32_t)(1<<g_APinDescription[ulPin].ulPin) ;
+
+  zt3.enable(true);
+/*********************************************/
+
+#endif
+
 
 adc_setup();
 startADCTimer(gADCPointsPerSec);
 }
 
-//volatile int32_t gLastADCus = 0;
+#ifdef TIMING_CHECK
+volatile int32_t gLastADCus = 0;
+int32_t gLastLastADCus = 0;
+#endif
 
 void ADC0_1_Handler()
 {
-//gLastADCus = micros();
+#ifdef TIMING_CHECK
+gLastADCus = micros();
+#endif
+digitalWrite(6, gADCstate = !gADCstate );  
 
 int val = ADC0->RESULT.reg;
 
@@ -377,7 +586,7 @@ else
    syncADC0_INPUTCTRL();
    }
 
-digitalWrite(6, gADCstate = !gADCstate );  
+//digitalWrite(6, gADCstate = !gADCstate );  
 }
 
 class PacketBase
@@ -553,16 +762,17 @@ ftPacket.write(Serial);
 debugNewLine();   //Readability while testing only!
 }
 
-//int32_t gLastLastADCus = 0;
 
 void loop()
 {
-// int32_t delta = gLastADCus - gLastLastADCus;
-// if(delta > 0)
-//    {
-//    Serial.println("  delta = "+ String(delta));
-//    gLastLastADCus = gLastADCus;
-//    }
+#ifdef TIMING_CHECK
+int32_t delta = gLastADCus - gLastLastADCus;
+if(delta > 0)
+   {
+   Serial.println("  delta = "+ String(delta));
+   gLastLastADCus = gLastADCus;
+   }
+#endif
 
 int hasRx = Serial.peek();
 
